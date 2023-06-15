20123a09819230eb23d6aeb331a887a5a6e9b56f
Majed Karimi <m.karimi@Banimode.local>
2023-06-15 19:10:16 +0430

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import Nav from "@/components/Nav";
+import "../styles/globals.css";
+// meta data for this page layout
 export const metadata = {
-  titie: "next",
-  description: "majed",
+  titie: "promtopia",
+  description: "discover share api",
 };
 interface prop {
   children: React.ReactNode;
 }
+/**
+ *# why we import nav and proidrer ?
+ * cuz layout meen in this page we need this UI in different page
+ */
 const RootLayout: React.FC<prop> = ({ children }) => {
   return (
-    <html>
-      <body>{children}</body>
+    <html lang="end">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
     </html>
   );
 };
---
+++ components/Nav.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+const Nav = () => {
+  return <div>Nav</div>;
+};
+
+export default Nav;
